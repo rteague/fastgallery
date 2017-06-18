eee2d30a2f4afe3231e54eca724b01f770e44520
rteague <rashaud.teague@gmail.com>
2017-06-17 19:35:13 -0500

    for development add droptables.sql

---
+++ init/droptables.sql
@@ -0,0 +1,10 @@
+
+drop table if exists `tags`;
+drop table if exists `settings`;
+drop table if exists `galleries_photos`;
+drop table if exists `galleries`;
+drop table if exists `photos`;
+drop table if exists `pages`;
+drop table if exists `accounts`;
+
+
