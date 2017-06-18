7e09dc3bda2787281517ecf23c97c156e6c2d5d9
rteague <rashaud.teague@gmail.com>
2017-06-18 03:56:22 -0500

    more route comments and dependency updates

--- api/index.php
+++ api/index.php
@@ -51,42 +51,49 @@ $app->get('/', function (Request $request, Response $response) {
 
 // /admin/accounts GET
 // /admin/accounts POST
+// /admin/accounts DELETE
 // /admin/accounts/{id} GET
 // /admin/accounts/{id} PUT
 // /admin/accounts/{id} DELETE
 
 // /admin/pages GET
 // /admin/pages POST
+// /admin/pages DELETE
 // /admin/pages/{id} GET
 // /admin/pages/{id} PUT
 // /admin/pages/{id} DELETE
 
 // /admin/photos GET
 // /admin/photos POST
+// /admin/photos DELETE
 // /admin/photos/{id} GET
 // /admin/photos/{id} PUT
 // /admin/photos/{id} DELETE
 
 // /admin/galleries GET
 // /admin/galleries POST
+// /admin/galleries DELETE
 // /admin/galleries/{id} GET
 // /admin/galleries/{id} PUT
 // /admin/galleries/{id} DELETE
 
 // /admin/tags GET
 // /admin/tags POST
+// /admin/tags DELETE
 // /admin/tags/{id} GET
 // /admin/tags/{id} PUT
 // /admin/tags/{id} DELETE
 
 // /admin/categories GET
 // /admin/categories POST
+// /admin/categories DELETE
 // /admin/categories/{id} GET
 // /admin/categories/{id} POST
 // /admin/categories/{id} DELETE
 
 // /admin/subscribers GET
 // /admin/subscribers POST
+// /admin/subscribers DELETE
 // /admin/subscribers/{id} GET
 // /admin/subscribers/{id} POST
 // /admin/subscribers/{id} DELETE
