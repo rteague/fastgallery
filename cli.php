dfa1943308208d069a4ec060449a469ff3c20702
rteague <rashaud.teague@gmail.com>
2017-06-21 21:01:44 -0500

    added stuff

--- cli.php
+++ cli.php
@@ -7,6 +7,17 @@
  *
  */
 
+// models
+spl_autoload_register(function ($classname) {
+    require ('models/' . $classname . '.php');
+});
 
+// controllers
+spl_autoload_register(function ($classname) {
+    require ('controllers/' . $classname . '.php');
+});
 
+$a = new Account;
+
+print $a->delete(Account::HARD_DELETE);
 
